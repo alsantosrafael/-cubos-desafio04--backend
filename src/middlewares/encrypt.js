3ff05a1b143a1f5fdf752eb844702b0d0ca53d27
Rafael Almeida <rafael.profeng@gmail.com>
2020-11-17 19:54:48 -0300

    Fix(encrypt.js): 'response' require was fixed.

--- src/middlewares/encrypt.js
+++ src/middlewares/encrypt.js
@@ -1,5 +1,5 @@
 const Password = require('../utils/password');
-const response = require('../controllers/response');
+const response = require('../utils/response');
 
 /**
  * Função responsável por encriptar a senha do usuário quando ele for cadastrado
@@ -10,7 +10,7 @@ const encrypt = async (ctx, next) => {
 	const { senha = null } = ctx.request.body;
 
 	if (!senha) {
-		return response(ctx, 'Pedido mal formatado.', 400);
+		return response(ctx, 'Pedido mal formatado.', 400, 'Erro!');
 	}
 	// Através do interceptador estou encriptografando a senha
 	const hash = await Password.encrypt(senha);
