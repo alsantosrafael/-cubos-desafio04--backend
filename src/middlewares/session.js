0c23deee0ebd9597cedaa471e0f37f0fbe4eebe2
Rafael Almeida <rafael.profeng@gmail.com>
2020-11-17 20:08:02 -0300

    Feat(session.js): Added new feature 'session' using JWT.

---
+++ src/middlewares/session.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const response = require('../utils/response');
+
+// require('dotenv').config();
+
+const verifica = async (ctx, next) => {
+	const [, token] = ctx.headers.authorization.split(' ');
+
+	try {
+		const verificacao = await jwt.verify(token, process.env.JWT_SECRET);
+
+		ctx.state.userId = verificacao.id;
+		ctx.state.email = verificacao.email;
+	} catch (err) {
+		return response(ctx, 'Ação proibida', 403, 'Erro!');
+	}
+	return next();
+	}
+};
+module.exports = { verifica };
