1e3c8c0c45a9bf9edcfb44af8192805206b92165
Rafael Almeida <rafael.profeng@gmail.com>
2020-11-21 10:48:48 -0300

    FEAT(./controllers/users.js): Criada a função de 'criarUsuario' em controllers.

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,3 +1,27 @@
-const criarUsuario = (ctx) => {};
+const Users = require('../repositories/users');
+const response = require('../utils/response');
+
+const criarUsuario = (ctx) => {
+	const { email = null, nome = null } = ctx.request.body;
+	const { hash } = ctx.state;
+	if (!email || !nome) {
+		return response(ctx, 400, { messagem: 'Pedido mal-formatado!' });
+	}
+
+	const existeUsuario = await Users.obterUsuarioPorEmail(email);
+
+	if(existeUsuario) {
+		return response(ctx, 400, { messagem: 'E-mail de usuário já cadastrado!' });
+	}
+
+	const usuario = {email, senha: hash, nome};
+
+	const result = await Users.criarUsuario(usuario);
+
+	/*Adicionar email de confirmação/registro */
+
+	return response(ctx, 201, result)
+
+};
 
 module.exports = { criarUsuario };
--- src/utils/response.js
+++ src/utils/response.js
@@ -2,13 +2,12 @@
  * Função responsável por obter informações e formatar resposta à requisições
  */
 
-const response = (ctx, dados, codeStatus = 200, mensagem = 'Sucesso!') => {
+const response = (ctx, dados, codeStatus = 200) => {
 	const status = codeStatus >= 200 && codeStatus <= 399 ? 'Sucesso' : 'Erro';
 	ctx.status = codeStatus;
 	ctx.body = {
 		status,
 		dados,
-		mensagem,
 	};
 };
 
