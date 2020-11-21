babd7a7846316a4530d3ce1b02cb1a9a00002358
Rafael Almeida <rafael.profeng@gmail.com>
2020-11-21 09:44:18 -0300

    FEAT(repositories/users.js): added functiont 'criarUsuario'.

--- src/repositories/users.js
+++ src/repositories/users.js
@@ -1,9 +1,17 @@
 const db = require('../utils/database');
 
 const criarUsuario = async (usuario) => {
-	const {email, senha, nome} = usuario;
-	const query = {text: `INSERT INTO users
-						(email, senha, )`}
+	const { email, senha, nome } = usuario;
+	const query = {
+		text: `INSERT INTO users(
+				email, 
+				senha,
+				nome
+			) VALUES ($1, $2, $3) RETURNING *;`,
+		values: [email, senha, nome],
+	};
+	const result = await db.query(query);
+	return result.rows.shift();
 };
 
 module.exports = { criarUsuario };
