3b2c3639e3b897d486f03c756db1eef7b7beeea0
Felipe Isaac <felipeisaacferreira@gmail.com>
2020-11-26 21:23:31 -0300

    fix(paginacao): correção de bug na função de cálculo de paginação

--- src/utils/paginacao.js
+++ src/utils/paginacao.js
@@ -1,8 +1,12 @@
 const calcularPaginas = (lista, itensPorPagina, offset) => {
 	const paginaAtual = offset % itensPorPagina +1;
 	const paginasTotais = Math.ceil(lista.length % itensPorPagina);
-	const itensDaPagina = lista.slice(offset+1, offset + itensPorPagina);
-	return [paginaAtual, paginasTotais, itensDaPagina];
+	const itensDaPagina = lista.slice(offset, offset + itensPorPagina);
+	return {
+		paginaAtual,
+		paginasTotais,
+		itensDaPagina
+	};
 };
 
 module.exports = calcularPaginas
