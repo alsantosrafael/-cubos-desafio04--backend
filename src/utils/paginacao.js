32b40523948d406e9cdb6c13bb2c6d1d3c877277
Felipe Isaac <felipeisaacferreira@gmail.com>
2020-11-28 15:42:45 -0300

    fix(clients): correção na função que define a paginação

--- src/utils/paginacao.js
+++ src/utils/paginacao.js
@@ -2,7 +2,7 @@ const calcularPaginas = (lista, itensPorPagina, offset) => {
 	
 	const paginaAtual = offset % itensPorPagina +1;
 	const paginasTotais = Math.ceil(lista.length / itensPorPagina);
-	const itensDaPagina = lista.slice(offset, offset + itensPorPagina);
+	const itensDaPagina = lista.slice(offset, Number(offset) + Number(itensPorPagina));
 	
 	return {
 		paginaAtual,
